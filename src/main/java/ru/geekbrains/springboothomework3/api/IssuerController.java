17ad2befba7f5e4469e935cf4ccfad32e532fefe
AleksOstr <ostryankoam@yandex.ru>
2024-01-17 21:50:32 +0400

    IssuerController added getIssueById

--- src/main/java/ru/geekbrains/springboothomework3/api/IssuerController.java
+++ src/main/java/ru/geekbrains/springboothomework3/api/IssuerController.java
@@ -3,10 +3,7 @@ package ru.geekbrains.springboothomework3.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.springboothomework3.api.request.IssueRequest;
 import ru.geekbrains.springboothomework3.model.Issue;
 import ru.geekbrains.springboothomework3.service.IssuerService;
@@ -19,29 +16,39 @@ import java.util.NoSuchElementException;
 public class IssuerController {
 
 
-  private IssuerService service;
+    private IssuerService service;
 
-  public IssuerController(IssuerService service) {
-    this.service = service;
-  }
+    public IssuerController(IssuerService service) {
+        this.service = service;
+    }
 
-  //  @PutMapping
+    //  @PutMapping
 //  public void returnBook(long issueId) {
 //    // найти в репозитории выдачу и проставить ей returned_at
 //  }
 
-  @PostMapping
-  public ResponseEntity<Issue> issueBook(@RequestBody IssueRequest request) {
-    log.info("Получен запрос на выдачу: readerId = {}, bookId = {}", request.getReaderId(), request.getBookId());
+    @PostMapping
+    public ResponseEntity<Issue> issueBook(@RequestBody IssueRequest request) {
+        log.info("Получен запрос на выдачу: readerId = {}, bookId = {}", request.getReaderId(), request.getBookId());
 
-    final Issue issue;
-    try {
-      issue = service.issue(request);
-    } catch (NoSuchElementException e) {
-      return ResponseEntity.notFound().build();
+        final Issue issue;
+        try {
+            issue = service.issue(request);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(issue);
     }
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(issue);
-  }
+    @GetMapping("/{id}")
+    public ResponseEntity<Issue> getIssueById(@PathVariable Long id) {
+        try {
+            Issue issue = service.getIssueById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(issue);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
