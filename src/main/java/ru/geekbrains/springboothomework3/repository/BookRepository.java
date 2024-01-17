a5c48df994048887ad30e9037cb7f764a5bd320d
AleksOstr <ostryankoam@yandex.ru>
2024-01-17 20:44:54 +0400

    BookRepository upograded

--- src/main/java/ru/geekbrains/springboothomework3/repository/BookRepository.java
+++ src/main/java/ru/geekbrains/springboothomework3/repository/BookRepository.java
@@ -11,25 +11,33 @@ import java.util.Objects;
 @Repository
 public class BookRepository {
 
-  private final List<Book> books;
+    private final List<Book> books;
 
-  public BookRepository() {
-    this.books = new ArrayList<>();
-  }
+    public BookRepository() {
+        this.books = new ArrayList<>();
+    }
 
-  @PostConstruct
-  public void generateData() {
-    books.addAll(List.of(
-      new Book("война и мир"),
-      new Book("метрвые души"),
-      new Book("чистый код")
-    ));
-  }
+    @PostConstruct
+    public void generateData() {
+        books.addAll(List.of(
+                new Book("война и мир"),
+                new Book("метрвые души"),
+                new Book("чистый код")
+        ));
+    }
 
-  public Book getBookById(long id) {
-    return books.stream().filter(it -> Objects.equals(it.getId(), id))
-      .findFirst()
-      .orElse(null);
-  }
+    public Book getBookById(long id) {
+        return books.stream().filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void insertBook(Book book) {
+        books.add(book);
+    }
+
+    public void delete(long id) {
+        books.remove(getBookById(id));
+    }
 
 }
