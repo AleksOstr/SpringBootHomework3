53f5bcb964246afac3bafda29fecbfcda4523606
AleksOstr <ostryankoam@yandex.ru>
2024-01-26 22:55:13 +0400

    IssueRepository updated

--- src/main/java/ru/geekbrains/springboothomework3/repository/IssueRepository.java
+++ src/main/java/ru/geekbrains/springboothomework3/repository/IssueRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.springboothomework3.model.entity.IssueEntity;
 
+import java.util.List;
+
 @Repository
 public interface IssueRepository extends JpaRepository<IssueEntity, Long> {
+    List<IssueEntity> findAllByReaderIdAndReturnedAtNull(Long id);
+    List<IssueEntity> findAllByReaderId(Long id);
 }
