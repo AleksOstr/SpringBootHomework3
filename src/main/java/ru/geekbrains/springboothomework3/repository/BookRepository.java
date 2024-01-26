278468f61d114218d04f38fed9f106dfd6830e22
AleksOstr <ostryankoam@yandex.ru>
2024-01-26 22:10:12 +0400

    corrections

--- src/main/java/ru/geekbrains/springboothomework3/repository/BookRepository.java
+++ src/main/java/ru/geekbrains/springboothomework3/repository/BookRepository.java
@@ -1,7 +1,9 @@
 package ru.geekbrains.springboothomework3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.geekbrains.springboothomework3.model.entity.BookEntity;
 
+@Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
 }
--- src/main/java/ru/geekbrains/springboothomework3/repository/ReaderRepository.java
+++ src/main/java/ru/geekbrains/springboothomework3/repository/ReaderRepository.java
@@ -1,7 +1,9 @@
 package ru.geekbrains.springboothomework3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.geekbrains.springboothomework3.model.entity.ReaderEntity;
 
+@Repository
 public interface ReaderRepository extends JpaRepository<ReaderEntity, Long> {
 }
