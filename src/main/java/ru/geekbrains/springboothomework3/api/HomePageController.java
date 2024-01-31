38e87b2d625a28239e268814b1c777b62ae25240
AleksOstr <ostryankoam@yandex.ru>
2024-01-31 19:28:39 +0400

    HomePageController described

--- src/main/java/ru/geekbrains/springboothomework3/api/HomePageController.java
+++ src/main/java/ru/geekbrains/springboothomework3/api/HomePageController.java
@@ -1,11 +1,19 @@
 package ru.geekbrains.springboothomework3.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Tag(name = "Homepage")
 public class HomePageController {
     @GetMapping("/home")
+    @Operation(summary = "Returns homepage")
+    @ApiResponse(responseCode = "200", description = "Homepage returned",
+            content = @Content(mediaType = "text/html"))
     public String homePage() {
         return "index";
     }
