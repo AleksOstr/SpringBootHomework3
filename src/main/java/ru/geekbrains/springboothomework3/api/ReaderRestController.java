38dc2409a3757994fd74220b6699bcb6332f2322
AleksOstr <ostryankoam@yandex.ru>
2024-01-31 19:44:12 +0400

    ReaderRestController described

--- src/main/java/ru/geekbrains/springboothomework3/api/ReaderRestController.java
+++ src/main/java/ru/geekbrains/springboothomework3/api/ReaderRestController.java
@@ -1,5 +1,12 @@
 package ru.geekbrains.springboothomework3.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +18,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
+@Tag(name = "Reader")
 public class ReaderRestController {
     private final ReaderService readerService;
 
@@ -19,13 +27,22 @@ public class ReaderRestController {
     }
 
     @GetMapping("/api/readers")
+    @Operation(summary = "Get all readers")
+    @ApiResponse(responseCode = "200", description = "Get the list of readers", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ReaderEntity.class))})
     public ResponseEntity<List<ReaderEntity>> getReaders() {
         List<ReaderEntity> readers = readerService.findAll();
         return ResponseEntity.ok(readers);
     }
 
     @GetMapping("/api/readers/{id}")
-    public ResponseEntity<ReaderEntity> findById(@PathVariable Long id) {
+    @Operation(summary = "Get reader by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get the reader", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ReaderEntity.class))}),
+            @ApiResponse(responseCode = "404", description = "Reader not found")
+    })
+    public ResponseEntity<ReaderEntity> findById(@Parameter(description = "ID of target reader") @PathVariable Long id) {
         try {
             ReaderEntity reader = readerService.findById(id);
             return ResponseEntity.ok(reader);
