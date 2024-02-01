35f84d5e861e6693cdbab8760b267d6637dd3ca8
AleksOstr <ostryankoam@yandex.ru>
2024-02-01 21:58:51 +0400

    updated

--- src/main/java/ru/geekbrains/springboothomework3/security/UserRepository.java
+++ src/main/java/ru/geekbrains/springboothomework3/security/UserRepository.java
@@ -1,9 +1,19 @@
 package ru.geekbrains.springboothomework3.security;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByLogin(String login);
+
+    @PostConstruct
+    private void posConstruct() {
+        save(new User("admin", "admin", List.of(new Role("admin"), new Role("reader"))));
+        save(new User("reader", "reader", List.of(new Role("reader"))));
+        save(new User("user", "user", List.of(new Role("user"))));
+    }
 }
