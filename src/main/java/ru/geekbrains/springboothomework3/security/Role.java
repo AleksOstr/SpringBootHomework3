a717e2aa7d41cb2f2c399d89e19d0ff30027c20b
AleksOstr <ostryankoam@yandex.ru>
2024-02-01 20:58:49 +0400

    Role class created

---
+++ src/main/java/ru/geekbrains/springboothomework3/security/Role.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.springboothomework3.security;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
