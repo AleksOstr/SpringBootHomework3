22b9e71915b16bbc45506d213bafae94e59e7e80
AleksOstr <ostryankoam@yandex.ru>
2024-02-01 20:58:55 +0400

    User class created

---
+++ src/main/java/ru/geekbrains/springboothomework3/security/User.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.springboothomework3.security;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")}
+    )
+    private List<Role> roles;
+}
