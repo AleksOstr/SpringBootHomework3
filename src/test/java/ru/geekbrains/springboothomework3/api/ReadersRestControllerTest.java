256cf8cc9aaa7f7e800e1cab79ad1986028cda88
AleksOstr <ostryankoam@yandex.ru>
2024-02-14 20:55:42 +0400

    ReadersRestControllerTest completed

--- src/test/java/ru/geekbrains/springboothomework3/api/ReadersRestControllerTest.java
+++ src/test/java/ru/geekbrains/springboothomework3/api/ReadersRestControllerTest.java
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import ru.geekbrains.springboothomework3.api.request.ReaderRequest;
 import ru.geekbrains.springboothomework3.model.entity.ReaderEntity;
 import ru.geekbrains.springboothomework3.repository.ReaderRepository;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
@@ -21,6 +25,9 @@ public class ReadersRestControllerTest {
 
     @Autowired
     ReaderRepository readerRepository;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     void saveTest() {
@@ -41,5 +48,65 @@ public class ReadersRestControllerTest {
         Assertions.assertEquals(request.getName(), response.getName());
 
         Assertions.assertTrue(readerRepository.findById(response.getId()).isPresent());
+
+        readerRepository.deleteAll();
+    }
+
+    @Test
+    void getReadersTest() {
+        List<ReaderEntity> expected = readerRepository.saveAll(List.of(
+                new ReaderEntity("First"),
+                new ReaderEntity("Second")
+        ));
+
+        List<ReaderEntity> response = webClient.get()
+                .uri("/api/readers")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<List<ReaderEntity>>(){})
+                .returnResult().getResponseBody();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(expected.size(), response.size());
+
+        for (ReaderEntity reader : response) {
+            boolean found = expected.stream().filter(it -> it.getId().equals(reader.getId()))
+                    .anyMatch(it -> it.getName().equals(reader.getName()));
+            Assertions.assertTrue(found);
+        }
+
+        readerRepository.deleteAll();
+    }
+
+    @Test
+    void findByIdTestOk() {
+        ReaderEntity expectedReader = readerRepository.save(new ReaderEntity("Reader"));
+
+        ReaderEntity response = webClient.get()
+                .uri("/api/readers/" + expectedReader.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(ReaderEntity.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(expectedReader.getId(), response.getId());
+        Assertions.assertEquals(expectedReader.getName(), response.getName());
+
+        readerRepository.deleteAll();
+    }
+
+    @Test
+    void findByIdTestNotFound() {
+        ReaderEntity expectedReader = readerRepository.save(new ReaderEntity("Reader"));
+
+        Long maxId = jdbcTemplate.queryForObject("SELECT max(id) FROM READER", Long.class);
+
+        webClient.get()
+                .uri("/api/readers/" + maxId + "1")
+                .exchange()
+                .expectStatus().isNotFound();
+
+        readerRepository.deleteAll();
     }
 }
